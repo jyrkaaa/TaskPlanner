fb2aaf1bc989aa4700505d503e64480a0713ff25
Jürgen Kuusik <jyrka55@gmail.com>
2026-06-08 21:52:58 +0300

    LEft over code

--- react_fe/src/pages/IssueDetailsPage.tsx
+++ react_fe/src/pages/IssueDetailsPage.tsx
@@ -1,18 +1,13 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useAlert } from "../context/AlertContext";
-import { IssueDto } from "../models/dto/IssueDto";
-import { issuesApi } from "../api/issues";
 
 function IssueDetailsPage() {
   const { tenantId, id } = useParams<{ tenantId: string, id: string }>();
   const { permissions } = useAuth();
   const {info} = useAlert();
   const navigate = useNavigate();
-  const [paginationSize, setPaginationSize] = useState(10);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [issues, setIssues] = useState<IssueDto[] | null>(null);
 
   useEffect(() => {
     if (hasFaultyParams() || !permissions) {
@@ -24,14 +19,7 @@ function IssueDetailsPage() {
       info("You do not have permissions for this tenant");
       navigate('/');
     }
-
-    issuesApi.getIssues(parseInt(tenantId!), { page: currentPage, size: paginationSize })
-      .then(response => setIssues(response.content))
-      .catch(error => {
-        info("Failed to fetch issues");
-        navigate('/');
-      });
-  }, [permissions, tenantId, navigate, currentPage, paginationSize]);
+  }, [permissions, tenantId]);
   
   function hasFaultyParams() {
     return !tenantId || isNaN(parseInt(tenantId)) || !id || isNaN(parseInt(id));
